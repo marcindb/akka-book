8e1fa34dac1410dd90218feb1b0daa13e191a163
marcindb <mdebow@gmail.com>
2014-01-11 17:45:38 +0100

    Chapter 8.5

--- src/main/scala/zzz/akka/avionics/Altimeter.scala
+++ src/main/scala/zzz/akka/avionics/Altimeter.scala
@@ -1,8 +1,10 @@
 package zzz.akka.avionics
 
 import akka.actor.{Props, Actor, ActorSystem, ActorLogging}
-
 import scala.concurrent.duration._
+import akka.actor.ActorRef
+import akka.actor.OneForOneStrategy
+import akka.actor.SupervisorStrategy._
 
 object Altimeter {
 	case class RateChange(amount: Float)
@@ -30,12 +32,29 @@ class Altimeter extends Actor with ActorLogging {
 
   var rateOfClimb = 0f
 
-  var altitude = 0f
+  var altitude = 0.0
 
   var lastTick = System.currentTimeMillis
 
   val ticker = context.system.scheduler.schedule(100.millis, 100.millis, self, Tick)
-
+  
+  case class CalculateAltitude(lastTick: Long, tick: Long, roc: Double)
+  
+  case class AltitudeCalculated(newTick: Long, altitude: Double)
+  
+  override def supervisorStrategy = OneForOneStrategy(-1, Duration.Inf){
+    case _ => Restart
+  }
+  
+  val altitudeCalculator = context.actorOf(Props(
+      new Actor{
+        def receive = {
+          case CalculateAltitude(lastTick, tick, roc) =>
+            val alt = ((tick - lastTick) / 60000.0) * roc
+            sender !  AltitudeCalculated(tick, alt)
+        }
+      }))
+  
   case object Tick
 
   def altimeterReceive: Receive = {
@@ -44,8 +63,10 @@ class Altimeter extends Actor with ActorLogging {
   	   log.info(s"Altimeter changed rate of climb $rateOfClimb")
   	case Tick =>
   	   val tick = System.currentTimeMillis
-  	   altitude = altitude + ((tick - lastTick)/60000.0f) * rateOfClimb
+  	   altitudeCalculator ! CalculateAltitude(lastTick, tick, rateOfClimb)
   	   lastTick = tick  	
+  	case AltitudeCalculated(tick, altdelta) =>
+  	   altitude += altdelta
   	   sendEvent(AltitudeUpdate(altitude))
   }
 
